29360f1f2afc520ea25a9e6fb800604dbf1e5d2f
Andrew Mishunin <51997374+Kourgeek@users.noreply.github.com>
2024-07-17 18:49:43 +0000

    Delete unused

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import fastapi
 import numpy as np
 import pandas as pd
-#from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression
 
-#define model info
+#import data
 
-model_name  =  "Simple_model"
-model_path = "Simple_ML_model.ipynb"
+data = pd.read_csv("data.csv")
+data = data[["Age","Gender","Ethnicity","Absences","GradeClass"]]
+
+# train | target
+X = data[["Age","Gender","Ethnicity","Absences"]]
+y =  data ["GradeClass"]
+
+# train model
+reg = LinearRegression().fit(X,y)
+
+#X = [18, 1,1,10]
 
 app = fastapi.FastAPI()
 
@@ -14,6 +23,6 @@ app = fastapi.FastAPI()
 def check_api():
     return "WORK"
 
-@app.get("/score")
-def get_GPA_score(X):
-    return 1
+@app.post("/score")
+def get_GPA_score(X: list):
+    return float(reg.predict(np.array([X])))
